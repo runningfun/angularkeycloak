ad5cfebeef87a3de984978205dfc2b2f1944c214
runningfun <stefan.herschbach@gmail.com>
2017-10-25 09:31:48 +0200

    add npm i angular-oauth2-oidc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { UserComponent } from './user/user.component';
+import {UserService} from './user/user.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+             selector: 'app-root',
+             templateUrl: './app.component.html',
+             styleUrls: ['./app.component.css'],
+             providers: [UserService]
+           })
+export class AppComponent implements OnInit{
+
+
+  title = 'Coffee list app works!';
+  users: UserComponent[];
+  selectedUser: UserComponent;
+  constructor(private userService: UserService) { }
+
+  getUsers(): void {
+    this.users = this.userService.getUsers();
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  // user: UserComponent = {
+  //   id: 1,
+  //   name: 'Stefan'
+  // }
+  // user2: UserComponent = {
+  //   id: 2,
+  //   name: 'Stefan2'
+  // }
+
+  onSelect(user: UserComponent): void {
+    this.selectedUser = user;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+            declarations: [
+              AppComponent,
+              UserlistComponent,
+              UserComponent
+            ],
+            imports: [
+              BrowserModule,
+              FormsModule,
+              HttpModule
+            ],
+            providers: [],
+            bootstrap: [AppComponent]
+          })
 export class AppModule { }
