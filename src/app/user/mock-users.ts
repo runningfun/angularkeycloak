cae51e0d281f6d4ddb3d471a2227a05da33ee024
runningfun <stefan.herschbach@gmail.com>
2017-10-28 19:28:45 +0200

    change index.html

--- src/app/user/mock-users.ts
+++ src/app/user/mock-users.ts
@@ -7,4 +7,5 @@ export const USERS: UserComponent[] = [
   {id: 1, accountName: 'myAccount', email: 'info@stefan-herschbach.de', firstName: 'stefan', lastName: 'test'},
   {id: 2, accountName: 'willy', email: 'info@stefan-willy.de', firstName: 'steve', lastName: 'Willy'},
   {id: 3, accountName: 'harry', email: 'info@harry.de', firstName: 'harry', lastName: 'Potter'},
+  {id: 4, accountName: 'hermine', email: 'info@hermine.de', firstName: 'hermins', lastName: 'granger'},
 ];
