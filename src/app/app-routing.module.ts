f3462ab427b971d292cf7581d1644d3a5f0dc931
runningfun <stefan.herschbach@gmail.com>
2017-10-29 20:24:11 +0100

    add css styling

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {UserDetailComponent} from './user/userdetail.component';
+import {UsersComponent} from './users/users.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'detail/:id', component: UserDetailComponent },
+  { path: 'users',     component: UsersComponent }
+];
+
+@NgModule({
+            imports: [ RouterModule.forRoot(routes) ],
+            exports: [ RouterModule ]
+          })
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {UrlHelperService} from 'angular-oauth2-oidc';
 import {UsersComponent} from './users/users.component';
 import {UserService} from './user/user.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
             declarations: [
@@ -22,24 +23,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
               BrowserModule,
               FormsModule,
               HttpModule,
-              RouterModule.forRoot([
-                                     { path: '',
-                                       redirectTo: '/dashboard',
-                                       pathMatch: 'full'
-                                     },
-                                     {
-                                       path: 'users',
-                                       component: UsersComponent
-                                     },
-                                     {
-                                       path: 'detail/:id',
-                                       component: UserDetailComponent
-                                     },
-                                     {
-                                       path: 'dashboard',
-                                       component: DashboardComponent
-                                     }
-                                   ])
+              AppRoutingModule,
             ],
             providers: [UrlHelperService, UserService],
             bootstrap: [AppComponent]
